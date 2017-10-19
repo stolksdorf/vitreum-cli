730efdde1d0ed19b27fbb2cb05ba6fde8359efab
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-10-19 16:16:08 -0400

    'Added

--- bin/vitreum/vitreum.templates.js
+++ bin/vitreum/vitreum.templates.js
@@ -19,5 +19,18 @@ test('My first test', (t)=>{
 `;
 	},
 
+	testReact : ()=>{
+		return `const React  = require('react');
+const test   = require('tape');
+const render = (comp) => require('react-test-renderer').create(comp).toJSON();
+
+test('renders the button', (t) => {
+	const btn = render(<button className='test' />);
+	t.equals(btn.type, 'button');
+	t.equals(btn.props.className, 'test');
+	t.end();
+});`;
+	}
+
 
 }
