67df5740975f05952590ee5faaf175e458d919a1
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-10-19 11:23:36 -0400

    Splitting up the templates, added a bunch of new features

--- bin/vitreum/vitreum.templates.js
+++ bin/vitreum/vitreum.templates.js
@@ -1,41 +1,61 @@
+const fs = require('fs');
 
 module.exports = {
+	scripts : require('./script.templates.js'),
+	eslint : ()=>fs.readFileSync('./eslint.template.js', 'utf8'),
+
+
 
 /** Server **/
 
 	server : ()=>{
-		return `const _       = require('lodash');
-const config  = require('nconf');
+		return `const config = require('nconf');
 const express = require('express');
 const app     = express();
-app.use(express.static(\`\${__dirname}/build\`));
+app.use(require('cookie-parser')());
+app.use(express.static('./build'));
+app.enable('trust proxy');
 
-config.argv()
-	.env({lowerCase: true})
-	.file('environment', {file: \`config/\${process.env.NODE_ENV}.json\`})
-	.file('defaults', {file: 'config/default.json'});
+app.use(require('./basic.router.js'));
 
-const pageTemplate = require('./page.template.js');
 const render = require('vitreum/steps/render');
-
-app.get('*', (req, res)=>{
+const pageTemplate = require('./page.template.js');
+app.get('*', (req, res) => {
 	render('main', pageTemplate, {
-		url : req.url
-	})
-		.then((page)=>res.send(page))
-		.catch((err)=>console.log(err));
+			url    : req.url,
+			config : config.get('client')
+		})
+		.then((page) => res.send(page))
+		.catch((err) => console.log(err));
 });
 
-const PORT = config.get('port');
-app.listen(PORT);
-console.log(\`server on port:\${PORT}\`);
+module.exports = app;
 `;
 	},
 
+	app : ()=>{
+		return `const config = require('nconf')
+	.env({lowerCase: true, seperator: '.'})
+	.argv()
+	.file('environment', {file: \`config/\${process.env.NODE_ENV}.json\`})
+	.file('defaults', {file: 'config/default.json'});
+
+const server = require('./server/server.js');
+
+const PORT = config.get('port');
+server.listen(PORT, () => {
+	console.log(\`server on port:\${PORT}\`);
+});`
+
+	},
+
+	router: ()=>{
+
+	}
+
 /** Template **/
 	template : ()=>{
-		return `
-module.exports = (vitreum)=>{
+		return `module.exports = (vitreum)=>{
 	return \`<html>
 	<head>
 		<link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
@@ -53,51 +73,7 @@ module.exports = (vitreum)=>{
 
 
 
-/**  Build Script **/
-	buildScript : ()=>{
-		return `
-const label = 'build';
-console.time(label);
 
-const _     = require('lodash');
-const steps = require('vitreum/steps');
-const Proj  = require('./project.json');
-const {libs, shared} = Proj;
-
-Promise.resolve()
-	.then(()=>steps.clean())
-	.then(()=>steps.libs(Proj.libs))
-	.then(()=>Promise.all(_.map(Proj.entryPoints, (path, name)=>{
-		return steps.jsx(name, path, {libs, shared})
-			.then((deps)=>steps.less(name, {shared}, deps));
-	})))
-	.then(()=>steps.assets(Proj.assets, shared))
-	.then(()=>console.timeEnd(label))
-	.catch((err)=>console.error(err));`;
-	},
-
-/**  Dev Script **/
-	devScript : ()=>{
-		return `
-const label = 'dev';
-console.time(label);
-
-const _     = require('lodash');
-const steps = require('vitreum/steps');
-const Proj  = require('./project.json');
-const {libs, shared} = Proj;
-
-Promise.resolve()
-	.then(()=>Promise.all(_.map(Proj.entryPoints, (path, name)=>{
-		return steps.jsxWatch(name, path, {libs, shared})
-			.then((deps)=>steps.lessWatch(name, {shared}, deps));
-	})))
-	.then(()=>steps.assetsWatch(Proj.assets, shared))
-	.then(()=>steps.livereload())
-	.then(()=>steps.serverWatch('./server/server.js', ['server']))
-	.then(()=>console.timeEnd(label))
-	.catch((err)=>console.error(err));`;
-	},
 
 
 /**  Test **/
@@ -111,74 +87,7 @@ test('My first test', (t)=>{
 `;
 },
 
-/**  ESlint **/
-	eslint : ()=>{
-		return `
-{
-	"parserOptions": {
-		"ecmaVersion": 6,
-		"sourceType": "script",
-		"ecmaFeatures": {
-			"jsx": true,
-			"experimentalObjectRestSpread": true
-		}
-	},
-	"plugins": ["react"],
-	"rules" : {
-		/** Errors **/
-		"no-new-object": "error",
-		"no-array-constructor": "error",
-		"no-iterator": "error",
-		"no-proto": "error",
-		"no-nested-ternary": "error",
-		"func-style": ["error", "expression", {"allowArrowFunctions": true}],
-		"camelcase": ["error", {"properties": "never"}],
-		"react/prefer-es6-class": ["error", "never"],
-		"react/jsx-no-bind" : ["error", {"allowArrowFunctions" : true}],
 
-		/** Warnings **/
-		"no-restricted-syntax": ["warn", "ClassDeclaration", "SwitchStatement"],
-		"max-lines" : ["warn", {"max": 250, "skipComments": true, "skipBlankLines": true}],
-		"max-params" : ["warn", {"max": 4}],
-
-		/** Fixable **/
-		"quotes": ["warn", "single"],
-		"prefer-template": "warn",
-		"arrow-parens": ["warn", "always"],
-		"no-var": "warn",
-		"prefer-const": "warn",
-		"semi": ["warn", "always"],
-		"indent": ["warn", "tab"],
-		"linebreak-style": ["warn", "unix"],
-		"jsx-quotes": ["warn", "prefer-single"],
-
-		/** Whitespace **/
-		"template-curly-spacing": ["warn", "never"],
-		"arrow-spacing": ["warn", { "before": false, "after": false }],
-		"space-in-parens": ["warn", "never"],
-		"no-trailing-spaces": "warn",
-		"object-curly-spacing": ["warn", "never"],
-		"space-before-blocks": ["warn", "never"],
-		"no-whitespace-before-property": "warn",
-		"space-in-parens": ["warn", "never"],
-		"comma-spacing": ["warn", {"before": false, "after": true}],
-		"object-curly-spacing": ["warn", "never"],
-		"keyword-spacing": ["warn", {
-			"before": false, "after": false,
-			"overrides" : {
-				"return" : { "before" : true, "after" : true }
-			}
-		}],
-		"key-spacing": ["warn", {
-			"multiLine": {
-				"beforeColon": true,
-				"afterColon": true,
-				"align": "colon"
-			}
-		}]
-	}
-}`;
-	},
 
 
 }
